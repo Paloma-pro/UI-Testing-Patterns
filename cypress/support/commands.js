c888ea37ed99836a86053d1bf789a932016febcd
Palomadev <paloma166.pah@gmail.com>
2025-08-11 13:55:21 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,8 +28,8 @@ import { homePage } from "./pages/home.page"
 import loginPage from "./pages/login.page"
 
 Cypress.Commands.add('login', (email, senha) => {
-    // cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
-    cy.visit('/')
+    cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br/' })
+    cy.visit()
     homePage.openMenu('Account')
     loginPage.login(email, senha)
     homePage.openMenu('Account')
